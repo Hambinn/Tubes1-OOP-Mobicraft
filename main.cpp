f73b57a7260ee5e9bc10a6d9fcce3158159fe114
Hambinn <ilhambintangn@gmail.com>
2022-03-22 23:26:36 +0700

    hapus comment dan unused metod

--- main.cpp
+++ main.cpp
@@ -75,57 +75,6 @@ int main() {
     }
     delete command;
     cout << "Program ended." << endl;
-    // //Print List Non Tool (For Debug)
-    // cout << "Item Non Tool Available : "<< endl;
-    // for(int i=0; i < listItemNonTool.size(); i++)
-    // cout << listItemNonTool[i].getID() << ", " << listItemNonTool[i].getName() << ", " << listItemNonTool[i].getType() << endl;
-
-    // //Print List Non Tool (for Debug)
-    // cout << "\nItem Tool Available : " << endl;
-    // for(int i=0; i < listItemNonTool.size(); i++)
-    // cout << listItemTool[i].getID() << ", " << listItemTool[i].getName() << endl;
-
-//   // read recipes
-//   for (const auto &entry :
-//        filesystem::directory_iterator(configPath + "/recipe")) {
-//     cout << entry.path() << endl;
-//     // read from file and do something
-//   }
-
-//   // sample interaction
-//   string command;
-//   while (cin >> command) {
-//     if (command == "EXPORT") {
-//       string outputPath;
-//       cin >> outputPath;
-//       ofstream outputFile(outputPath);
-
-//       // hardcode for first test case
-//       outputFile << "21:10" << endl;
-//       outputFile << "6:1" << endl;
-//       for (int i = 2; i < 27; i++) {
-//         outputFile << "0:0" << endl;
-//       }
-
-//       cout << "Exported" << endl;
-//     } else if (command == "CRAFT") {
-//       cout << "TODO" << endl;
-//     } else if (command == "GIVE") {
-//       string itemName;
-//       int itemQty;
-//       cin >> itemName >> itemQty;
-//       cout << "TODO" << endl;
-//     } else if (command == "MOVE") {
-//       string slotSrc;
-//       int slotQty;
-//       string slotDest;
-//       // need to handle multiple destinations
-//       cin >> slotSrc >> slotQty >> slotDest;
-//       cout << "TODO" << endl;
-//     } else {
-//       // todo
-//       cout << "Invalid command" << endl;
-//     }
-//   }
+    
   return 0;
 }
